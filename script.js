6154e9057676953a2f7389777ee74f3ffbbfe513
Md-Kutub-Islam <mdkutubislam09@gamil.com>
2024-01-24 23:50:41 +0530

    cursor work

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// cursor work 
+const cursor = document.querySelector("#cursor")
+const cursorBlur = document.querySelector("#cursor-blur")
+document.addEventListener('mousemove', (dets) => {
+  cursor.style.left = dets.x+"px"
+  cursor.style.top = dets.y+"px"
+  cursorBlur.style.left = dets.x-150+"px"
+  cursorBlur.style.top = dets.y-150+"px"
+})
+
+
+
+
+
 gsap.to("#nav", {
     backgroundColor: "#000",
     duration: 0.5,
@@ -11,3 +25,17 @@ gsap.to("#nav", {
       scrub: 1,
     },
   });
+
+
+  // cursor work 
+  gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+      trigger: "#main",
+      scroller: "body",
+      markers: true,
+      start: "top -25%",
+      end: "top -70%",
+      scrub: 2
+    }
+  })
